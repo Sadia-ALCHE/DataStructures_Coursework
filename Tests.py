cb17f55608d67c72a40f76a263555b927776beea
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-09-11 22:57:41 +0400

    Implemented test function

---
+++ Tests.py
@@ -0,0 +1,10 @@
+# Let's test it now
+
+from Selection_Sort import Selection_Sort
+from Merge_Sort import Merge_Sort
+
+test_case = {
+    'sorted' : [1, 2, 3, 4, 5],
+    'unsorted' : [5, 4, 1, 3, 2],
+    'with_repeated_values' : [2, 4, 6, 1, 2, 3, 5, 4]
+}
