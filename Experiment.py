6676d9aa451754add7916636eeeb160c2e6e31e7
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-09-13 00:56:48 +0400

    Formatted to make code readable

--- Experiment.py
+++ Experiment.py
@@ -12,6 +12,7 @@ sizes_to_test = [500, 1000, 2000, 4000, 8000]
 print("List of Selection Sort times:")
 for n in sizes_to_test:
     times = []               # Creating an empty list to store the 5 running times
+
     for i in range(5):
         # Creating a random list containing n numbers
         test_list = [random.randint(1, 10000) for _ in range(n)]
@@ -19,6 +20,7 @@ for n in sizes_to_test:
         selection_sort(test_list)        # Runs the selection sort algorithm
         end = time.perf_counter()        # Stops the timer after sorting is done
         times.append(end - start)        # Calculates the difference between the starting and ending time and storing it
+
     mean_time = sum(times) / 5          # Calculates average running time
 
     print(f"n = {n}")                   # Print the input size and its average running time
@@ -29,12 +31,14 @@ for n in sizes_to_test:
 print("List of Merge Sort times:")
 for n in sizes_to_test:
     times = []
+
     for i in range(5):
         test_list = [random.randint(1, 10000) for _ in range(n)]
         start = time.perf_counter()
         merge_sort(test_list)           # Runs the merge sort algorithm
         end = time.perf_counter()
         times.append(end - start)
+
     mean_time = sum(times) / 5
 
     print(f"n = {n}")                    # Print the input size and its average running time
@@ -44,8 +48,11 @@ for n in sizes_to_test:
 
 print("Selection Sort on already sorted lists:")
 for n in sizes_to_test:
-    test_list = list(range(n))
+    test_list = list(range(n))    # Create a sorted list of numbers starting from 0
+
     start = time.perf_counter()
     selection_sort(test_list)      # Runs the selection sort on already sorted data
     end = time.perf_counter()
-    print(f"n = {n}: {end - start:.6f} seconds")
+
+    print(f"n = {n}")
+    print(f"time_taken = {end - start:.6f} seconds")
